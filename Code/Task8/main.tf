d61c02b78ef0c161ec75e78bb7560b5f8645abe9
A7ryan <aryanpandya2310@gmail.com>
2026-07-18 21:43:36 +0530

    Task 8 AMI from Existing Ec2

---
+++ Code/Task8/main.tf
@@ -0,0 +1,20 @@
+# Provision EC2 instance
+resource "aws_instance" "ec2" {
+  ami           = "ami-0c101f26f147fa7fd"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [
+    "sg-fbefbe8d052ce4fcc"
+  ]
+
+  tags = {
+    Name = "datacenter-ec2"
+  }
+}
+
+resource "aws_ami_from_instance" "datacenter_ec2_ami" {
+    name = "datacenter-ec2-ami"
+    source_instance_id = aws_instance.ec2.id
+    tags = {
+    Name = "datacenter-ec2-ami"
+  }
+}
